736e95eded02078b88ab0118e69a57d42b2a1dcc
ShakthivelMDU <136778664+ShakthivelMDU@users.noreply.github.com>
2026-06-12 18:10:46 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI(title="InvoiceGuard AI", version="1.0.0")
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://invoicedetector.netlify.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {
